d8a5c8ebe8944c5ee7f2e6bc230043c019486c74
Alexander Sharov <dronline872@gmail.com>
2023-06-18 17:37:41 +0300

    Lesson 1

---
+++ Lesson/task_1.java
@@ -0,0 +1,28 @@
+package lessons.Lesson;
+
+// 1) Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+public class task_1 {
+    public static void main(String[] args) {
+        // треугольное число
+        System.out.println(triangular(4));
+        // факториал
+        System.out.println(factorial(4));
+    }
+
+    static int triangular(int n) {
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result = result + i;
+        }
+
+        return result;
+    }
+
+    static int factorial(int n) {
+        if (n < 2) {
+            return 1;
+        } 
+
+        return n * factorial(n-1);
+    }
+}
